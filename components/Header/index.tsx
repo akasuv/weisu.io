62172b6ae3b1445228f629575bf15dbbbe2b5fd9
Wei Su <me@weisu.io>
2023-03-18 21:52:01 +0800

    fix: use width in class

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,7 +5,7 @@ import logo from "@/public/logo.svg";
 const Header = () => (
   <div className="h-[60px] py-8 flex justify-between">
     <Link href="/">
-      <Image src={logo} width={25} alt="weisu.io-logo" className="h-[25px]" />
+      <Image src={logo} alt="weisu.io-logo" className="h-[25px] w-[25px]" />
     </Link>
     <div className="flex gap-x-4">
       <Link href="https://twitter.com/aka_suv">Twitter</Link>
