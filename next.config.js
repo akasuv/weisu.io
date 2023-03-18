24a68f916151d541c006c36cb979ce85583ada78
Wei Su <me@weisu.io>
2023-03-18 21:25:54 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   reactStrictMode: true,
+=======
+  experimental: {
+    appDir: true,
+  },
+>>>>>>> 1645751 (Initial commit from Create Next App)
 }
 
 module.exports = nextConfig
