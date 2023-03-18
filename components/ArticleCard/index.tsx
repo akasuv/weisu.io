8cb002feead2613a6d20e08ed739a1bc4bee0625
Wei Su <me@weisu.io>
2023-03-18 21:57:02 +0800

    fix: change date style

--- components/ArticleCard/index.tsx
+++ components/ArticleCard/index.tsx
@@ -11,7 +11,7 @@ const ArticleCard = ({ link, title, created_at }: ArticleCardProps) => {
     <Link href={`/article/${encodeURIComponent(link)}`}>
       <div className="flex flex-col gap-2">
         <p className="group-hover:underline">{title}</p>
-        <p className="text-sm text-zinc-600">{created_at.split("T")[0]}</p>
+        <p className="text-xs text-zinc-300">{created_at.split("T")[0]}</p>
       </div>
     </Link>
   );
